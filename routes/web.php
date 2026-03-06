2ae316de16ff9c2c5c64c8b7c18a0fbf5d6b7392
Manuel Calderon <manuelcalderon5890@gmail.com>
2026-03-06 17:30:45 -0500

    feat(breeze) instalacion y ejecucion de breeze

--- routes/web.php
+++ routes/web.php
@@ -1,40 +1,25 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\LibroController;
-use App\Http\Controllers\CategoriasController;
-
-
-// estas rutas es la forma profesional de hacerla mas rapido y eficiente ya que redirije todas las rutas a un controlador y este se encarga de manejar cada una de las acciones, como crear, editar, eliminar, etc.
-
-// Route::resource('libros', LibroController::class);
-// Route::resource('categorias', CategoriasController::class);
-
-
-// ruta de la landing page
-
-Route::get('/', function () {return view('welcome');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-
-// rutas de las categorias
-
-Route::get('/categorias', [CategoriasController::class, 'index']);
-Route::get('/categorias/create', [CategoriasController::class, 'create']);
-Route::post('/categorias', [CategoriasController::class, 'store']);
-Route::get('/categorias/show/{categorias}', [CategoriasController::class, 'show']);
-Route::get('/categorias/edit/{categorias}', [CategoriasController::class, 'edit']);
-Route::put('/categorias/update/{categorias}', [CategoriasController::class, 'update']);
-Route::delete('/categorias/delete/{categorias}', [CategoriasController::class, 'destroy']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 
-// rutas de los libros
+// las rutas estan encapsuladas en un grupo de middleware 'auth' para asegurar que solo los usuarios autenticados puedan acceder a ellas
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-Route::get('/libros', [LibroController::class, 'index']);
-Route::get('/libros/create', [LibroController::class, 'create']);
-Route::post('/libros', [LibroController::class, 'store']);
-Route::get('/libros/show/{libro}', [LibroController::class, 'show']);
-Route::get('/libros/edit/{libro}', [LibroController::class, 'edit']);
-Route::put('/libros/update/{libro}', [LibroController::class, 'update']);
-Route::delete('/libros/delete/{libro}', [LibroController::class, 'destroy']);
+    // rutas para categorias
+    
+});
+
+require __DIR__.'/auth.php';
